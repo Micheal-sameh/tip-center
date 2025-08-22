87b1599b7f9e44128c793edd68baa71f43f8adc9
micheal <micheal.s.samir@gmail.com>
2025-08-22 16:12:16 +0300

    student index have success message and request message

--- app/Http/Requests/StudentCreateRequest.php
+++ app/Http/Requests/StudentCreateRequest.php
@@ -12,11 +12,43 @@ class StudentCreateRequest extends FormRequest
         return [
             'name' => 'required|string|unique:students,name',
             'stage' => 'required|integer|in:'.implode(',', array_column(StagesEnum::all(), 'value')),
-            'phone' => 'required_without:parent_phone|string|unique:students,phone,',
-            'parent_phone' => 'required_without:phone|string',
-            'parent_phone_2' => 'string',
+            'phone' => 'required_without:parent_phone|string|min:11|max:11|unique:students,phone,',
+            'parent_phone' => 'required_without:phone|min:11|max:11|string',
+            'parent_phone_2' => 'string|min:11|max:11',
             'birth_date' => 'date',
             'note' => 'string',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Student name is required.',
+            'name.string' => 'Student name must be a valid string.',
+            'name.unique' => 'This student name already exists.',
+
+            'stage.required' => 'Stage is required.',
+            'stage.integer' => 'Stage must be an integer value.',
+            'stage.in' => 'Invalid stage selected.',
+
+            'phone.required_without' => 'Phone number is required if parent phone is not provided.',
+            'phone.string' => 'Phone number must be a valid string.',
+            'phone.min' => 'Phone number must be exactly 11 digits.',
+            'phone.max' => 'Phone number must be exactly 11 digits.',
+            'phone.unique' => 'This phone number is already registered.',
+
+            'parent_phone.required_without' => 'Parent phone is required if student phone is not provided.',
+            'parent_phone.string' => 'Parent phone must be a valid string.',
+            'parent_phone.min' => 'Parent Phone number must be exactly 11 digits.',
+            'parent_phone.max' => 'Parent Phone number must be exactly 11 digits.',
+
+            'parent_phone_2.string' => 'Parent second phone must be a valid string.',
+            'parent_phone_2.min' => 'Parent second phone must be exactly 11 digits.',
+            'parent_phone_2.max' => 'Parent second phone must be exactly 11 digits.',
+
+            'birth_date.date' => 'Birth date must be a valid date.',
+
+            'note.string' => 'Note must be a valid string.',
         ];
     }
 }
